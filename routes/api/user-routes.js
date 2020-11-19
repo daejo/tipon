c6eafd28d931f88848074f404ba5508e2d3e71d3
daejo <daejo@icloud.com>
2020-11-19 02:30:13 -0800

    sets up create user route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,2 +1,8 @@
 const { User, Thought } = require('../../models');
+const router = require('express').Router();
 
+router.post('/', ({ body }, res) => {
+    User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
+})
