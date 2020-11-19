5a281d2343c9a80d1dcd96ca4456b96e3ae826a7
daejo <daejo@icloud.com>
2020-11-19 00:37:41 -0800

    adds comments into thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,2 +1,15 @@
 const { Schema, model } = require('mongoose');
 
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1, // sets minimum character length for strings in mongoDB. 
+        maxlength: 280 // sets maximum character length for strings in mongoDB. 
+    },
+    createdAt: {
+        type: Date, // set as date for type.
+        default: Date.now, // sets current date.
+        get: ((createdAtVal) => dateFormat(createdAtVal)) // getter method to convert date value into a more readable syntax.
+    }
+})
