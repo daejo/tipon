cc512e2af5555f4682b1e5842f37744dec784475
daejo <daejo@icloud.com>
2020-11-19 22:16:11 -0800

    creates the post user routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,20 +1,42 @@
 const { User, Thought } = require('../../models');
 const router = require('express').Router();
 
-// searches for all the pizza.
+//========== SEARCH USER ==========//
+// searches for all the users.
 router.get('/', (req, res) => {
     User.find({})
     .select('-__v') // gets rid of 'v' in the body
-    .then(dbUserData => res.json)
+    .then(dbUserData => res.json(dbUserData))
     .catch(err => {
         console.log(err);
         res.status(400).json(err);
     });
 });
+// searches for a single user.
+router.get('/:id', ({ params }, res) {
+    User.findOne({ _id: params.id})
+    .select('-__v')
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' })
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    })
+});
 
+//========== MAKE USER ==========//
 // sets up a new user.
 router.post('/', ({ body }, res) => {
     User.create(body)
         .then(dbUserData => res.json(dbUserData))
         .catch(err => res.status(400).json(err));
 });
+
+//========== EDIT USER ==========//
+
+
