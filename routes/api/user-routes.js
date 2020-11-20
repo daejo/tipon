44eae4e30a2b47ee1bf4a25ae369b89ee344b6ed
daejo <daejo@icloud.com>
2020-11-19 22:04:35 -0800

    adds a user search route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,8 +1,20 @@
 const { User, Thought } = require('../../models');
 const router = require('express').Router();
 
+// searches for all the pizza.
+router.get('/', (req, res) => {
+    User.find({})
+    .select('-__v') // gets rid of 'v' in the body
+    .then(dbUserData => res.json)
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+});
+
+// sets up a new user.
 router.post('/', ({ body }, res) => {
     User.create(body)
         .then(dbUserData => res.json(dbUserData))
         .catch(err => res.status(400).json(err));
-})
+});
